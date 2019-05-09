66138187832d40216af974c1f981305e0ccca71e
Leonardo Cintra <leonardo.ncintra@outlook.com>
2019-05-09 16:19:19 -0300

    add princing page

--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 class IndexView(TemplateView):
-    template_name = 'core/index.html'
+    template_name = "core/index.html"
+
+
+class PricingView(TemplateView):
+    template_name = "core/pricing.html"
